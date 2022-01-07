84f3220a98500827aff5d8629273c0f955d4ce64
Luke-Avveduto <Luke.Avveduto@mail.utoronto.ca>
2022-01-07 17:30:17 -0500

    Added comments to all the current methods in Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,6 +1,10 @@
 import java.time.LocalDateTime;
 
-
+/**
+ * This class represents an item stored in the inventory. Each time contains a name as a String, the date the item was
+ * first stored in the inventory, how many of the items are in the inventory and when that last time that quantity was
+ * updated. Dates are stored using <java.time.LocalDateTime></> objects.
+ */
 public class Item {
 
     private String name;
@@ -11,8 +15,8 @@ public class Item {
     /**
      * This creates a new item that can be stored in the inventory
      * @param name the name of the item stored as string
-     * @param dateEntered the date the item was created stored using a <java.time.LocalDateTime> object
-     * @param dateModified the date the item was last modified stored using a <java.time.LocalDateTime> object
+     * @param dateEntered the date the item was created stored using a <java.time.LocalDateTime></> object
+     * @param dateModified the date the item was last modified stored using a <java.time.LocalDateTime></> object
      * @param quantity the number of the same item stored in the inventory represented as an int
      */
     public Item(String name, LocalDateTime dateEntered, LocalDateTime dateModified, int quantity) {
@@ -22,24 +26,40 @@ public class Item {
         this.quantity = quantity;
     }
 
-
+    /**
+     * Returns the name of the item
+     * @return the name of the item as a String
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * Returns the quantity of items stored
+     * @return the quantity of items stored as an int
+     */
     public int getQuantity() {
         return quantity;
     }
+    /**
+     * Returns the date the item was first created
+     * @return the date the item was first created as a <java.time.LocalDateTime></> object
+     */
     public LocalDateTime getDateEntered() {
         return dateEntered;
     }
+    /**
+     * Returns the date the item was last modified
+     * @return the date the item was last modified as a <java.time.LocalDateTime></> object
+     */
     public LocalDateTime getDateModified() {
         return dateModified;
     }
 
-    public void setName(String name){
-        this.name = name;
-        this.dateModified = LocalDateTime.now();
-    }
+    /**
+     * Sets the quantity of the item
+     * @param quantity the new quantity of the item
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
         this.dateModified = LocalDateTime.now();
